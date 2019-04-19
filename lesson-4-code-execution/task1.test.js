f648a77c930a5e07a4851cb7279f7cb5913d00ec
Anastasiya Khikhnal <a.khikhnal@gmail.com>
2019-04-19 15:51:23 +0300

    Delete negativ test in task1.test.js, replace default value in task3.html

--- lesson-4-code-execution/task1.test.js
+++ lesson-4-code-execution/task1.test.js
@@ -21,14 +21,4 @@ describe('Maximum value', function isMaxValues() {
     var result = getMaxValue(obj);
     expect(result).toBe('Grisha:25');
   });
-  it('Negative test: to find the maximum value if value of object is string', function f() {
-    var obj = {
-      'Vasya': 20,
-      'Misha': 18,
-      'Dasha': '28',
-      'Grisha': 90
-    };
-    var result = getMaxValue(obj);
-    expect(result).toBe('Grisha:90');
-  });
 });
